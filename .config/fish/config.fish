285f01d53c6a79c457137e805ea2c87fc6f7cb05
Owieczka <owieczka@owieczka>
2024-01-12 18:05:24 +0100

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,23 @@
+###########################################
+#  _                    _             _   #
+# | |    __ _ _ __ ___ | |__       _-(")- #
+# | |   / _` | '_ ` _ \| '_ \    `%%%%%   #
+# | |__| (_| | | | |_| | |_) | _  // \\   #
+# |_____\__,_|_| |_| |_|_.__/_| |__  ___  #
+#                  | |   / _` | '_ \/ __| #
+#                  | |__| (_| | |_) \__ \ #
+#  2024-01-12      |_____\__,_|_.__/|___/ #
+###########################################
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+  # Custom pixelart
+  bash ~/.config/pixelart/lamb16.sh
+end
+
+# Dotfile repo adapter
+alias gitdotfiles="/usr/bin/git --git-dir=$HOME/dotfiles-linux-public-repo --work-tree=$HOME"
+alias gitprivate="/usr/bin/git --git-dir=$HOME/dotfiles-linux-private-repo --work-tree=$HOME"
